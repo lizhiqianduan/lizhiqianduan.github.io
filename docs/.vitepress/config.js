5055c4dbbf3f91f20577c482303caf40bd32a175
xiaohei <1024669228@qq.com>
2022-12-30 17:58:16 +0800

    deploy

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   title: '前端文档库 | 励志前端',
   description: '用于存放前端中文文档，用于整理记录个人心得',
   themeConfig:{
-    'logo':'./logo.jpg',
+    'logo':'/logo.jpg',
     nav: [
       { text: '首页', link: '/index.md'},
       { text: 'esbuild中文文档', link: '/esbuild/index.md'}
