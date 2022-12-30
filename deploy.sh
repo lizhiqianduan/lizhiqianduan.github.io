051b97db6793c436bb514888d58b317643233a7b
xiaohei <1024669228@qq.com>
2022-12-30 16:59:35 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ set -e
 # 构建
 npx vitepress build docs
 
-# 将构建后的产物，挪动到zh_cn目录
-rm -rf zh_cn/**
-mv docs/.vitepress/dist/** zh_cn/
+# 将构建后的产物，同步到根目录
+rsync -av docs/.vitepress/dist/** ./
+
+git add .
+git commit -m "deploy"
+git push
