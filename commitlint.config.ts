47e94c73d530e85ec6ac1d91766073f107aa1a3d
xiaohei <1024669228@qq.com>
2023-01-10 16:05:49 +0800

    site: 提交日志规范

---
+++ commitlint.config.ts
@@ -0,0 +1,31 @@
+import {UserConfig} from '@commitlint/types/lib';
+
+// /**
+//  * @type {import('@commitlint/cli')}
+//  */
+// module.exports = {
+//     extends: ['@commitlint/config-conventional'],
+//     rules: {
+//       'type-case': [2, 'always', ['lower-case', 'upper-case']],
+//       'type-enum': [2, 'always',['fix', 'site','esbuild']]
+//     }
+//   }
+
+
+
+  const Configuration: UserConfig = {
+    /*
+     * Resolve and load @commitlint/config-conventional from node_modules.
+     * Referenced packages must be installed
+     */
+    extends: ['@commitlint/config-conventional'],
+    /*
+     * Any rules defined here will override rules from @commitlint/config-conventional
+     */
+    rules: {
+      'type-case': [2, 'always', ['lower-case', 'upper-case']],
+      'type-enum': [2, 'always',['fix', 'site','esbuild']]
+    }
+  };
+  
+  module.exports = Configuration;
